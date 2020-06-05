7aeed81f78a22842062be16960e187ac917bd56d
Matheus <matheusalexandre.sena99@gmail.com>
2020-06-04 21:06:25 -0300

    Function to create inside query brackets structure

--- src/schema/schemaBuilder.js
+++ src/schema/schemaBuilder.js
@@ -1,4 +1,5 @@
 const types = require('./types.json')
+const queries = require('./queries.json')
 
 async function data(typeData) {
     let datas = []
@@ -11,6 +12,39 @@ async function data(typeData) {
     return datas.reduce(reducer)
 }
 
+async function buildParams(params) {
+    let str = "("
+    params.map((param, index) => {
+        if (index + 1 >= params.length) {
+            str += `${param.fieldName}: ${param.dataType}${param.required ? "!" : ""})`
+        } else {
+            str += `${param.fieldName}: ${param.dataType}${param.required ? "!" : ""},`
+        }
+    })
+
+    return str
+}
+
+async function buildReturn(returns) {
+    let str = ''
+    if (returns.type == "array") {
+        str = `[${returns.typeName}]`
+    } else if (returns.type == "normal") {
+        str= returns.typeName
+    }
+    return str
+}
+
+async function buildQueries() {
+    let str = ""
+
+    await queries.map(async query => {
+        str += `\n${query.queryName}${(query.queryParams) ? await buildParams(query.queryParams) : ''}: ${await buildReturn(query.return)}`
+        console.log(str)
+    })
+}
+buildQueries();
+
 module.exports = {
     buildType() {
         return new Promise(async (resolve, reject) => {
@@ -18,7 +52,7 @@ module.exports = {
                 let str = "";
 
                 await types.map(async type => {
-                    str += `type ${type.typeName} {\n${await data(type.typeData)}}\n`
+                    str += `\ntype ${type.typeName} {\n${await data(type.typeData)}}\n`
                 })
                 resolve(str)
 
