7ec50a42674ac397e5fb49dcca2c62a10a8546a0
Matheus <matheusalexandre.sena99@gmail.com>
2020-06-05 23:59:14 -0300

    simplify functions

--- src/schema/schemaBuilder.js
+++ src/schema/schemaBuilder.js
@@ -8,7 +8,7 @@ async function buildSchema() {
         mySchema += `
         type ${type.typeName}
         { 
-            ${await data(type.typeData)}
+            ${await data(type.typeData).reduce((a, b) => a + '\n' + b)}
         }\n`
     })
     // build all queries
@@ -21,15 +21,10 @@ async function buildSchema() {
     return (mySchema).replace(/  /g, '');
 }
 
-async function data(typeData) {
-    let datas = []
-
-    typeData.map(e => {
-        datas.push(`${e.fieldName}: ${e.dataType}${(e.required? "!" : "")}\n`)
+function data(typeData) {
+    return typeData.map(e => {
+        return `${e.fieldName}: ${e.dataType}${(e.required? "!" : "")}`
     })
-    const reducer = (a, b) => a + b;
-
-    return datas.reduce(reducer)
 }
 
 async function buildQueries() {
@@ -37,19 +32,14 @@ async function buildQueries() {
     const buildReturn = (returns) => (returns.type == "array") ? `[${returns.typeName}]` : returns.typeName
     const buildParams = (params) => params.map(param => `${param.fieldName}: ${param.dataType}${param.required ? "!" : ""}`)
 
-    let arrqueries = []
+    let arrayQueries = []
 
     await queries.map(async query => {
         const q = `${query.queryName}${(query.queryParams) ? '(' + await buildParams(query.queryParams) + ')' : ''}: ${await buildReturn(query.return)}`
-        arrqueries.push(q);
+        arrayQueries.push(q);
     })
 
-    return (arrqueries)
-
+    return (arrayQueries)
 }
 
-let x = buildSchema()
-x.then(y => {
-    console.log(y)
-
-}).catch(e => console.error(e))
+buildSchema().then(y => console.log(y)).catch(e => console.error(e))
