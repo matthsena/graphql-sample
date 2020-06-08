9e2404117298a8bf7af1112abd01fbcc7f23902c
Matheus <matheusalexandre.sena99@gmail.com>
2020-06-08 10:59:58 -0300

    Use basic auth to test middleware, in future we'll change to JWT

--- src/index.js
+++ src/index.js
@@ -5,7 +5,27 @@ const {
 } = require('./schema/schema')
 const rootResolver = require('./resolver/rootResolver')
 
+// Basic auth
+const loggingMiddleware = (req, res, next) => {
+
+    const auth = {
+        login:'yourlogin',
+        password: 'yourpassword'
+    } 
+    
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+    const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+
+    if (login && password && login === auth.login && password === auth.password) {
+        return next()
+    }
+    res.set('WWW-Authenticate', 'Basic realm="401"')
+    res.status(401).send('Authentication required.')
+}
+
 const app = express()
+
+app.use(loggingMiddleware);
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
