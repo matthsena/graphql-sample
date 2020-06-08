a2cb665932dbaa71d922477bde112c874729d116
Matheus <matheusalexandre.sena99@gmail.com>
2020-06-07 22:49:55 -0300

    Temp stop this way to generate schema

--- src/schema/schema.js
+++ src/schema/schema.js
@@ -2,28 +2,7 @@ const {
     buildSchema
 } = require('graphql')
 
-const { schemaBuilder } = require('./schemaBuilder')
-
-schemaBuilder().then(r => console.log(r))
-
-const schema = buildSchema(`
-    # Types
-    type Movie {
-        id: ID!
-        title: String!
-        year: Int!
-        duration: Int!
-        genre: String!
-        imdbRate: Float!
-        director: String
-        }
-
-    # Queries
-    type Query {
-        getMovies: [Movie]
-        bestMovie: Movie
-        searchMovie(_id: ID): Movie
-    }
-`)
+const schemaTemplate = require('./template.json')
+const schema = buildSchema(schemaTemplate.data)
 
 exports.schema = schema;
