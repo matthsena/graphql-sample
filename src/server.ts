6ca89afe6bda7b6c1b321e2708abe52598dbb2c6
Matheus <matheusalexandre.sena99@gmail.com>
2020-06-18 21:31:38 -0300

    mend

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,9 @@
-/* eslint-disable no-console */
 import app from './app';
-import { schema } from './schema/schema';
-// import rootResolver from './resolver/rootResolver';
-// import resolvers from './graphql/resolver/rootResolver';
+import schema from './graphql/schemas/schema';
 
 try {
   app.GraphQLServer(schema, '/teste');
-  app.express.listen(4000, () => {
-    console.log('running');
-    console.log(process.env.GRAPHIQL);
-  });
+  app.express.listen(4000);
 } catch (error) {
   throw new Error(error);
 }
